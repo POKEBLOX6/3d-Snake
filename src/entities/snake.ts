f2e101905a9b71ce08aa8fe5757fadc86c958383
ianfights <ianfights@gmail.com>
2022-04-07 12:45:43 -0400

    a

--- src/entities/snake.ts
+++ src/entities/snake.ts
@@ -1,27 +1,24 @@
 import { Actor, Color } from "excalibur";
-import { game } from "index";
+import { game } from "../index";
 
-class snake {
+
+class Snake extends Actor {
+    public length: number = 1;
+    public head: Actor;
+    public bodySegments: Actor[] = [];
     constructor(){
-        
+        super({
+            x:  game.drawWidth /2,
+            y: game.drawHeight /2,
+            width: 20,
+            height: 20,
+            color: Color.Green, 
+        });
+        game.add(this)
+        this.head = this
     }
+
+    
 }
 
-const paddle = new Actor({
-    x: 150,
-    y: game.drawHeight - 40,
-    width: 200,
-    height: 20,
-    // Let's give it some color with one of the predefined
-    // color constants
-    color: Color.Chartreuse,
-  });
-  
-  // Make sure the paddle can partipate in collisions, by default excalibur actors do not collide with each other
-  // CollisionType.Fixed is like an object with infinite mass, and cannot be moved, but does participate in collision.
-  //@ts-ignore
-  paddle.body.collisionType = CollisionType.Fixed;
-  
-  // `game.add` is the same as calling
-  // `game.currentScene.add`
-  game.add(paddle);
+export { Snake as Snake}
