6ad99deb1534ddc96d4344e0f2efe94b07f19bc3
ianfights <ianfights@gmail.com>
2022-04-13 23:05:18 -0400

    Basic movement code done

--- src/entities/snake.ts
+++ src/entities/snake.ts
@@ -1,4 +1,4 @@
-import { Actor, Color } from "excalibur";
+import { Actor, Color, Input, Timer } from "excalibur";
 import { game } from "../index";
 
 
@@ -6,19 +6,94 @@ class Snake extends Actor {
     public length: number = 1;
     public head: Actor;
     public bodySegments: Actor[] = [];
-    constructor(){
+    public currentDir: String
+    public started: Boolean;
+    public timer: Timer = new Timer({
+        fcn: () => this.moveHead(this.currentDir),
+        repeats: true,
+        interval: 150,
+    })
+    constructor() {
         super({
-            x:  game.drawWidth /2,
-            y: game.drawHeight /2,
+            x: game.drawWidth / 2,
+            y: game.drawHeight / 2,
             width: 20,
             height: 20,
-            color: Color.Green, 
+            color: Color.Green,
         });
         game.add(this)
+        this.currentDir = 'Up';
         this.head = this
+        this.started = false;
+        game.add(this.timer);
+        this.timer.pause();
+    }
+    /* 
+        Movement code 
+        Save current pos in a variable called prevPos
+        move head depending on key press
+        loop through each of the indices in prevPos and update their pos to the one above them (except for section right after head that will just get updated to head pos)
+    */
+    private moveHead(dir: String) {
+        switch (dir) {
+            case 'Up':
+                this.currentDir = dir;
+                this.pos.y = this.pos.y - this.height;
+                break;
+            case 'Down':
+                this.currentDir = dir;
+                this.pos.y = this.pos.y + this.height;
+                break;
+            case 'Left':
+                this.currentDir = dir;
+                this.pos.x = this.pos.x - this.width;
+                break;
+            case 'Right':
+                this.currentDir = dir;
+                this.pos.x = this.pos.x + this.width;
+                break;
+        }
     }
 
-    
+    public update(engine) {
+        // Check if the game has started and if so start the loop for movement
+        if(this.started){
+            this.timer.unpause();
+        }
+        // Yes I understand this could be a switch but for some reason it doesn't want to read logical operators
+        // So I have to do it this way unless I want to write two switch statements
+        if (engine.input.keyboard._keysDown == Input.Keys.W || engine.input.keyboard._keysDown == Input.Keys.Up) {
+            if (this.started === false) {
+                this.started = true;
+            }
+            this.currentDir = 'Up'
+        }
+
+        if (engine.input.keyboard._keysDown == Input.Keys.A || engine.input.keyboard._keysDown == Input.Keys.Left) {
+            if (this.started === false) {
+                this.started = true;
+            }
+            this.currentDir = 'Left'
+        }
+
+        if (engine.input.keyboard._keysDown == Input.Keys.D || engine.input.keyboard._keysDown == Input.Keys.Right) {
+            if (this.started === false) {
+                this.started = true;
+            }
+            this.currentDir = 'Right'
+        }
+
+        if (engine.input.keyboard._keysDown == Input.Keys.S || engine.input.keyboard._keysDown == Input.Keys.Down) {
+            if (this.started === false) {
+                this.started = true;
+            }
+            this.currentDir = 'Down'
+        }
+
+
+
+    }
+
 }
 
-export { Snake as Snake}
+export { Snake as Snake }
